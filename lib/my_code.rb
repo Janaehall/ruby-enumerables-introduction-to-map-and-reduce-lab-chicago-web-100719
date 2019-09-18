7e42e46c8de863bbffc21ded94b279bc5be5fc53
bionic-chipset-2986 <janaeelisa@gmail.com>
2019-09-18 22:05:41 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -37,3 +37,12 @@ def reduce_to_total(array, starting_point=0)
   end
   total
 end
+
+def reduce_to_all_true(array)
+  array.each do |item|
+    if item != true
+      return false
+    end
+  end
+  return true
+end
